1a093309bf0b914792d9bc1f252985d50bca37dc
Nikie <carolnikie51@gmail.com>
2023-12-18 16:46:48 +0300

    multiply_list_map test file.

---
+++ data-structures/multiply_list_map_tst.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+multiply_list_map = __import__('multiply_list_map').multiply_list_map
+
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)
+
