76c8b06841e1ca9930a0a94042719393f7cadd4e
Nikie <carolnikie51@gmail.com>
2023-12-18 15:57:37 +0300

    Functions that print some basic info about lists, sets, tuples and dictionaries

---
+++ python-data-structures/best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def best_score(a_dictionary):
+     
+    if a_dictionary:
+        max_dict = max(a_dictionary, key = a_dictionary.get)
+        return max_dict
+    else: 
+        return None
+
+
