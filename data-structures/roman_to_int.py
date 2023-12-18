379abc7074d566b1461d37b7bf137de6653f7c92
Nikie <carolnikie51@gmail.com>
2023-12-18 17:05:42 +0300

    A function that converts roman to an interger.

---
+++ data-structures/roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) !=str:
+        return 0
+
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    total = 0 
+    prev_value = 0
+
+    for numeral in reversed(roman_string):
+        current_value = roman_dict[numeral]
+        if current_value >= prev_value:
+            total += current_value
+        else:
+            total -= current_value
+        prev_value = current_value
+
+    return total
